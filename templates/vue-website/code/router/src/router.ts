3ad493f01fd2cee565aa340893ba21c13753ca70
Jeff Schofield <jeff@jeffschofield.com>
2022-06-30 22:23:12 -0400

    refactor(vue-website): Use glob imports to generate routes

--- templates/vue-website/code/router/src/router.ts
+++ templates/vue-website/code/router/src/router.ts
@@ -1,12 +1,38 @@
 import { createRouter, createMemoryHistory, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 export function createAppRouter() {
-	const routes:RouteRecordRaw[] = [
-		{ path: '/', component: () => import('./pages/index.vue') },
+	const route_children = new Map<string, RouteRecordRaw[]>()
+	const routes = Object.entries(import.meta.glob('./pages/**/*.vue')).reduce<RouteRecordRaw[]>((routes, [page_path, component]) => {
+		page_path = page_path.replace(/^\.\/pages|.vue$/g, '')
 
-		// 404
-		{ path: '/:pathMatch(.*)*', component: () => import('./pages/404.vue') }
-	]
+		let path = page_path.replace(/\/_([^/]*)/g, '/:$1')
+		if (path.slice(-5) == 'index') path = path.slice(0, -5)
+
+		if (path == '/404') routes.push({ path: '/:pathMatch(.*)*', component })
+		else {
+			const [_, ...path_parts] = page_path.split('/')
+
+			let existing_route_path = ''
+			let found_route_path = ''
+			for (let part of path_parts) {
+				existing_route_path += `/${part}`
+
+				if (route_children.has(existing_route_path)) found_route_path = existing_route_path
+				else if (found_route_path) break // break if we have found a descendent route's children already and this iteration found nothing
+			}
+
+			const children:RouteRecordRaw[] = []
+			route_children.set(page_path, children)
+
+			if (found_route_path) {
+				route_children.get(found_route_path)!.unshift({ path, children, component })
+			} else {
+				routes.unshift({ path, children, component })
+			}
+		}
+
+		return routes
+	}, [])
 
 	const history = import.meta.env.SSR ? createMemoryHistory() : createWebHistory()
 
